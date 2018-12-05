1f9affd296639d20e2c58afad5c81fe20b810be5
Mayur Raval <mayurraval@asite.com>
2018-12-05 10:10:06 +0530

    First Message

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Header from './src/component/header';
+import AlbumList from './src/component/AlbumList';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Header headerText = {'albums 123'}/>
+        <AlbumList/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
